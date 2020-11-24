cbfe6e8e57c98c462a38445cc52e2e722192a35a
palanglois <thefroggy27@gmail.com>
2020-11-24 16:20:54 +0100

    Updated bim-data

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
     opt.add_option("-i", "--input", "Path to the input obj mesh", "");
     opt.add_option("-o", "--output", "Path to the output directory", ".");
     opt.add_option("-c", "--input", "Path to the json file containing the camera positions", "");
-    opt.add_option("-n", "--number", "Number of ray to shoot per point of view", "10000");
+    opt.add_option("-n", "--number", "Total number of points to shoot", "10000");
     opt.add_option("-gt", "--groundtruth", "Output the ground truth mesh with colored classes");
     opt.add_option("-pov", "--pointofview", "Save the point of views for each point");
     opt.add_option("-so", "--save_objects", "Output the separated_images");
@@ -74,13 +74,14 @@ int main(int argc, char *argv[]) {
     Tree tree(triangles.begin(), triangles.end());
 
     //Random shoots from given point of views
+    int nbShootPerPov = nbShoot / pointOfViews.size();
     vector<Point> sampledPoints;
     vector<Point> pointOfViews2;
     vector<colorTuple> pointColors;
     map<Point, int> pointClasses;
-    pointColors.reserve(nbShoot*pointOfViews.size());
-    sampledPoints.reserve(nbShoot*pointOfViews.size());
-    pointOfViews.reserve(nbShoot*pointOfViews.size());
+    pointColors.reserve(nbShoot);
+    sampledPoints.reserve(nbShoot);
+    pointOfViews.reserve(nbShoot);
 
     default_random_engine generator;
     normal_distribution<double> distribution(0.0, 1.0);
@@ -89,7 +90,7 @@ int main(int argc, char *argv[]) {
         curPt++;
         cout << "Doing pt " << curPt << " out of " << pointOfViews.size() << endl;
 #pragma omp parallel for schedule(dynamic, 1)
-        for (int i = 0; i < nbShoot; i++) {
+        for (int i = 0; i < nbShootPerPov; i++) {
 
             // Compute random ray
             double x = distribution(generator);
